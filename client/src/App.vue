d19483416b1bd8f44d122d4f1c78c3d4e542b58f
mikk0git <you@example.com>
2023-07-15 19:52:22 +0200

    Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+import Search from './components/Search.vue'
 
 </script>
 
 <template>
+
+<Navbar />
+<Search/>
 
 </template>
 
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Navbar',
+  // Add any necessary properties, methods, or lifecycle hooks here
+});
+</script>
+
+<template>
+   <nav id="navbar">
+        <div id="navbarLogo"><img src="../assets/olxlogo.png" alt="Olx logo" width="70"></div>
+
+        <div id="navbarButtons">
+            <div id="navbarMessages" class="navbarButton" >Wiadomości</div>
+            <div id="navbarLikes" class="navbarButton">Polubione oferty</div>
+            <div id="navbarYourAccount" class="navbarButton">Twoje konto</div>
+            <div id="navbarNewItem" class="navbarButton">Dodaj ogłoszenie</div>
+        </div>
+
+    </nav>
+  </template>
+  
+
+  
