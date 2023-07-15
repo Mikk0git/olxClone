0111623a5ae1b115f9226d5a955a85508b3cfb24
mikk0git <you@example.com>
2023-07-15 21:45:04 +0200

    Searchbar template

---
+++ client/src/components/Search.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Search',
+  // Add any necessary properties, methods, or lifecycle hooks here
+});
+</script>
+
+<template> <div id="search">
+      
+    <div id="ItemSearchbar">
+      <input type="text" name="ItemSearchbar" id="ItemSearchbarInput" class="searchInput" placeholder="X ogłoszeń blisko Ciebie">
+
+    </div>
+    <div id="LocationMenu" ><input type="text" name="LocationMenu" class="searchInput" id="LocationMenuInput" placeholder="Cała Polska"></div>
+
+    <div id="SearchButton" ><button class="searchInput" >Szukaj</button></div>
+  </div>
+    
+  </template>
+  
+
+  
